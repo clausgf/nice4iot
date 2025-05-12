314d431cd02b8c4a62ad3cbcd562831d6e8b6b52
Finn-Thorge Dummer <f.dummer@ostfalia.de>
2025-05-12 13:09:35 +0200

    Implemented rudimentary forwarding

--- app/api/device.py
+++ app/api/device.py
@@ -1,12 +1,13 @@
-from fastapi import APIRouter, HTTPException, Header, Request, Response, status
+from fastapi import APIRouter, HTTPException, Header, Request, Response, status, Depends
 from fastapi.responses import JSONResponse
 
-import numbers
+import numbers,json,httpx
+from app.api.dependencies import DeviceAuthInfo,device_auth
 from app.core.telemetry.telemetry_util import get_tel
 from app.core.logging.logging_util import get_log
-from app.core.project import get_project
+from app.core.project import get_project,get_project_path
 from app.core.device import get_device
-
+from app.core.forwarding import forward
 from app.util import logger, is_valid_filename
 
 
@@ -17,11 +18,10 @@ router = APIRouter()
 ###############################################################################
         
 @router.post('/telemetry/{project_name}/{device_name}/{kind}')
-async def post_telemetry_with_names(project_name: str, device_name: str, kind: str, request: Request):
+async def post_telemetry_with_names(project_name: str, device_name: str, kind: str, request: Request, dev : DeviceAuthInfo = Depends(device_auth)):
     """
     Post telemetry data to the time series database.
     """
-    #TODO Auth
     if not is_valid_filename(project_name) or not is_valid_filename(device_name) or not is_valid_filename(kind):
         raise HTTPException(status_code=400, detail='Invalid project, device, or kind')
 
@@ -61,7 +61,7 @@ async def post_telemetry_with_names(project_name: str, device_name: str, kind: s
 # ****************************************************************************
 
 @router.post('/log/{project_name}/{device_name}')
-async def post_log_with_names(project_name: str, device_name: str, request: Request):
+async def post_log_with_names(project_name: str, device_name: str, request: Request, dev : DeviceAuthInfo = Depends(device_auth)):
     """
     Post log data to the time series database.
     """
@@ -100,7 +100,10 @@ async def get_forward_with_names(project_name: str, forwarding_name: str, reques
     """
     if not is_valid_filename(project_name) or not is_valid_filename(forwarding_name):
         raise HTTPException(status_code=400, detail='Invalid project or forwarding')
-    return Response(status_code=200)
+    data = await request.body()
+    headers = request.headers
+    forward_response = await forward(project_name,forwarding_name,data,headers,10)
+    return Response(status_code=forward_response.status_code,headers=forward_response.headers,content=forward_response.content)
 
 # @GetMapping(value = "/forward/{projectName}/{forwardingName}/**")
 # headers, body, request
